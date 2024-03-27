7ee104d08efd1742d809d75a1692a5c28072e81a
Russ-Moonride <140208843+Russ-Moonride@users.noreply.github.com>
2024-03-27 17:18:37 -0400

    Update Sunpower_Ziplist_app.py

--- Sunpower_Ziplist_app.py
+++ Sunpower_Ziplist_app.py
@@ -6,6 +6,11 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read()
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+df = conn.read(
+    worksheet="Sheet1",
+    ttl="10m",
+    usecols=[0, 1],
+    nrows=3,
+)
+
+st.write(df)
